d6b32d5d10278e3ecfddfd72a76d7cbf289e9ec6
SyedRehan13 <159714228+SyedRehan13@users.noreply.github.com>
2025-07-12 15:02:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function getWeather(event) {
-            event.preventDefault();
+    event.preventDefault();
 
-            let cityname = document.weather.city.value.trim();
-            let api_key = "c1ea15041c62187fe209a7e6a9e112cb";
-            let api_url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api_key}&units=metric`;
+    let cityname = document.weather.city.value.trim();
+    let api_key = "c1ea15041c62187fe209a7e6a9e112cb";
+    let api_url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api_key}&units=metric`;
 
-            fetch(api_url)
-                .then(function(response) {
-                    if (!response.ok) {
-                        throw new Error("City not found");
-                    }
-                    return response.json();
-                })
-                .then(function(data) {
-                    // display result in table
-                    let result = `
-    <table border="1" cellpadding="10" cellspacing="0" align = "center"
-           style="margin-top:20px; background-color: rgba(0, 0, 0, 0.6); color: white; border-radius: 8px;">
+    fetch(api_url)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // display result in table
+            let result = `
+    <table class = "result-table">
         <tr><th colspan="2">Weather Result</th></tr>
         <tr><td><b>City</b></td><td>${data.name}</td></tr>
         <tr><td><b>Temperature</b></td><td>${data.main.temp} °C</td></tr>
@@ -25,9 +24,9 @@ function getWeather(event) {
     </table>
 `;
 
-                    document.getElementById("weatherResult").innerHTML = result;
-                })
-                .catch(function(error) {
-                    document.getElementById("weatherResult").innerHTML = "<p style='color:red;'>❌ City not found!</p>";
-                });
-        }
+            document.getElementById("weatherResult").innerHTML = result;
+        })
+        .catch(function (error) {
+            document.getElementById("weatherResult").innerHTML = "<p style='color:red; font-family:Montserrat; font-weight:bold; font-size:20px;'>❌ City not found!</p>";
+        });
+}
